0103a138570b10eb0f8bd41b3d184a366d008a3e
DrInnovatio <turing1723@gmail.com>
2020-01-08 14:02:38 +1000

    Updates

---
+++ precache-manifest.22d60473e5101121a94d2c94d3308909.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16bf1dc57d9000ff8a0eb372a21fa626",
+    "url": "/Torent-Box-Office/index.html"
+  },
+  {
+    "revision": "5a7127e3b2e105cfb889",
+    "url": "/Torent-Box-Office/static/css/main.3cbc1529.chunk.css"
+  },
+  {
+    "revision": "0b00472211c10da921d4",
+    "url": "/Torent-Box-Office/static/js/2.43afd1b8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Torent-Box-Office/static/js/2.43afd1b8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5a7127e3b2e105cfb889",
+    "url": "/Torent-Box-Office/static/js/main.00e06831.chunk.js"
+  },
+  {
+    "revision": "a2340e0e6a4da4260acb",
+    "url": "/Torent-Box-Office/static/js/runtime-main.08b85bf2.js"
+  }
+]);
